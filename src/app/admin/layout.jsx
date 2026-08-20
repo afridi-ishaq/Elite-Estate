2ddf74de94507b4b8dc67494cd3dc28a7f5e2208
Ishaq Afridi <ishaqafridi7119@gmail.com>
2026-08-20 21:22:54 +0500

    new updates

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -1,11 +1,12 @@
 import AdminSidebar from "@/components/admin/AdminSidebar";
 
+
 export default function AdminLayout({
   children,
 }) {
   return (
     <div className="flex">
-      <AdminSidebar />
+      <AdminSidebar className="fixed left-0 top-0 bottom-0 h-full" />
 
       <div className="flex-1 bg-[#F8F7F4] min-h-screen">
         {children}
