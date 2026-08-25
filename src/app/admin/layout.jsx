a2990a0f4b1941ab1b8ea048165ecbdd3527dbb8
Ishaq Afridi <ishaqafridi7119@gmail.com>
2026-08-26 01:13:00 +0500

    new updates

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -5,15 +5,29 @@ import { ResizablePanelGroup, ResizablePanel } from "@/components/ui/resizable";
 
 export default function AdminLayout({ children }) {
   return (
-    <div className="flex h-screen w-screen overflow-hidden">
-      <ResizablePanelGroup direction="horizontal" className="h-full w-full">
-        <AdminSidebar />
-        <ResizablePanel defaultSize={80}>
-          <div className="h-full overflow-y-auto bg-[#F8F7F4] p-6">
-            {children}
-          </div>
-        </ResizablePanel>
-      </ResizablePanelGroup>
+    <div className="fixed inset-0 h-screen w-screen overflow-hidden flex flex-col bg-[#F8F7F4]">
+      {/* 
+        If you have a global Top Header/Navbar component, place it here.
+        It will stay permanently fixed at the top without moving.
+      */}
+
+      {/* Main Resizable Body Container */}
+      <div className="flex-1 relative w-full h-full overflow-hidden">
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="h-full w-full flex-1"
+        >
+          {/* Resizable Sidebar */}
+          <AdminSidebar />
+
+          {/* Main Dashboard Content Area */}
+          <ResizablePanel defaultSize={75} minSize={50}>
+            <main className="h-full w-full overflow-y-auto overflow-x-hidden p-6">
+              {children}
+            </main>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
     </div>
   );
 }
