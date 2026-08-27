3598e1addbd23216c658059384e321b7902eff63
Ishaq Afridi <ishaqafridi7119@gmail.com>
2026-08-27 20:30:52 +0500

    new updates on the /admin/properties

--- src/app/admin/properties/page.jsx
+++ src/app/admin/properties/page.jsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import Link from "next/link";
 import Container from "@/components/Container";
 import { getAdminProperties } from "@/lib/admin-property-service";
